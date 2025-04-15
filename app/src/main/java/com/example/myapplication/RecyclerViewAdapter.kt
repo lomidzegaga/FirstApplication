69c9ca74025d1a170de1327be80877f868d7e020
galomidze <galomidze@bog.ge>
2025-04-15 18:34:39 +0400

    Added vietTypes on recyclerView

--- app/src/main/java/com/example/myapplication/RecyclerViewAdapter.kt
+++ app/src/main/java/com/example/myapplication/RecyclerViewAdapter.kt
@@ -2,10 +2,13 @@ package com.example.myapplication
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.core.view.isVisible
 import androidx.recyclerview.widget.RecyclerView
-import com.example.myapplication.databinding.RecyclerSingleItemBinding
+import androidx.recyclerview.widget.RecyclerView.ViewHolder
+import com.example.myapplication.databinding.RecyclerSingleItemWithImageBinding
+import com.example.myapplication.databinding.RecyclerSingleItemWithoutImageBinding
 
-class RecyclerViewAdapter : RecyclerView.Adapter<RecyclerViewAdapter.ViewHolder>() {
+class RecyclerViewAdapter : RecyclerView.Adapter<ViewHolder>() {
     private var list = mutableListOf<String>()
 
     private val icons = listOf(
@@ -14,31 +17,75 @@ class RecyclerViewAdapter : RecyclerView.Adapter<RecyclerViewAdapter.ViewHolder>
         R.drawable.adb,
     )
 
+    companion object {
+        private const val WITH_IMAGE = 0
+        private const val WITHOUT_IMAGE = 1
+    }
+
     fun submitData(name: String) {
         list.add(name)
         notifyDataSetChanged()
     }
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder = ViewHolder(
-        RecyclerSingleItemBinding.inflate(
-            LayoutInflater.from(parent.context), parent, false
-        )
-    )
+    override fun getItemViewType(position: Int): Int {
+        return if (position % 2 == 0) WITHOUT_IMAGE else WITH_IMAGE
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder =
+        when (viewType) {
+            WITH_IMAGE -> ViewHolderWithImage(
+                RecyclerSingleItemWithImageBinding.inflate(
+                    LayoutInflater.from(parent.context),
+                    parent,
+                    false
+                )
+            )
+
+            WITHOUT_IMAGE -> ViewHolderWithoutImage(
+                RecyclerSingleItemWithoutImageBinding.inflate(
+                    LayoutInflater.from(parent.context),
+                    parent,
+                    false
+                )
+            )
+
+            else -> ViewHolderWithoutImage(
+                RecyclerSingleItemWithoutImageBinding.inflate(
+                    LayoutInflater.from(parent.context),
+                    parent,
+                    false
+                )
+            )
+        }
 
 
     override fun getItemCount(): Int = list.size
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        holder.bind()
+        val viewType = getItemViewType(position)
+
+        when (viewType) {
+            WITH_IMAGE -> (holder as ViewHolderWithImage).bind()
+            WITHOUT_IMAGE -> (holder as ViewHolderWithoutImage).bind()
+        }
     }
 
-    inner class ViewHolder(
-        private val binding: RecyclerSingleItemBinding
-    ) : RecyclerView.ViewHolder(binding.root) {
+    inner class ViewHolderWithImage(
+        private val binding: RecyclerSingleItemWithImageBinding
+    ) : ViewHolder(binding.root) {
 
         fun bind() = with(binding) {
             tvName.text = list[adapterPosition]
-            ivImage.setImageResource(icons[(0..2).random()])
+            ivImage.setImageResource(icons.random())
+        }
+    }
+
+    inner class ViewHolderWithoutImage(
+        private val binding: RecyclerSingleItemWithoutImageBinding
+    ) : ViewHolder(binding.root) {
+
+        fun bind() = with(binding) {
+            tvName.text = list[adapterPosition]
         }
     }
 }
